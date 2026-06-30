107e8f4110628efa0a25f402dbd03cf462c7eb24
xxiyeon <tiaqhfl@nate.com>
2026-06-30 10:07:56 +0900

    메서드

--- ch01/index3.ts
+++ ch01/index3.ts
@@ -8,4 +8,20 @@
 let user; // 타입: any 하지만 이렇게 사용한다면 사용하는 의미 없음(그럴바엔 자바스크립트)
 // let user: string;
 
-user = "";
+user = "이시연"; // 타입: string
+user.toUpperCase();
+
+user = 1000;
+user.toPrecision(); // 문자열 메서드 - 문자열을 전부 대문자로 변경(원본을 직접 변경하지 않고 새로운 문자열을 반환)
+// user.toUpperCase(); // 숫자 메서드 - 숫자를 지정한 유효 자릿수만큼 문자열로 변환
+// const price = 123.567
+// console.log(price.toPrecision(6)); = 123.5 지정한 자릿수는 타입 외에 는 포함하지 않음(소숫점 포함x)
+
+// 숫자가 작으면 지수 표기법이 같이 나올 수 있음
+// 1e+2 = 100 이라는 의미
+// console.log(price.toPrecision(1)); = "1e+2"
+// console.log(price.toPrecision(2)); = "1.1e+2"
+// console.log(price.toPrecision(3)); = "123"
+
+let a = 3 + "5";
+console.log(a);
